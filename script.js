4aea934efe6bb3a95333fb04f0404c32bff8112d
Echnaton-AI <tomkreugel@gmail.com>
2025-03-14 10:42:03 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,10 +1,1 @@
-function loadAssistant() {
-    const select = document.getElementById("assistantSelect");
-    const iframe = document.getElementById("assistantFrame");
-    const url = select.value;
-    if (url) {
-        iframe.src = url;
-    }
-}
-
 console.log("Echnaton AI geladen!");
