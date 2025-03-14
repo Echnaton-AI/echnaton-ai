008aa78da3c777ef2ef9344789137e75d5acbcad
Echnaton-AI <tomkreugel@gmail.com>
2025-03-14 11:06:01 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
+function openAssistant(url) {
+    // Open een pop-upvenster met specifieke instellingen
+    window.open(url, "AssistantWindow", "width=400,height=600,top=100,left=400,toolbar=no,location=no,menubar=no,scrollbars=yes,resizable=yes");
+}
+
 console.log("Echnaton AI geladen!");
